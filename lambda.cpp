18e6ade15e836f2de3a58f6aa4ed07792c01101e
Yaosen Lin <ylin@xcom-labs.com>
2021-07-02 14:55:12 -0700

    learn lambda expression, capture mode

---
+++ lambda.cpp
@@ -0,0 +1,27 @@
+// Learn-CPP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+
+        int dummy = 0;
+        auto f1 = [=]() { dummy = 1; };
+        auto f2 = [&]() { dummy = 2; };
+
+        std::cout << "before f1" << std::endl;
+        std::cout << "  dummy = " << dummy << std::endl;
+
+        f1();
+        std::cout << "after f1" << std::endl;
+        std::cout << "  dummy = " << dummy << std::endl;
+
+        std::cout << "before f2" << std::endl;
+        std::cout << "  dummy = " << dummy << std::endl;
+
+        f2();
+        std::cout << "after f2" << std::endl;
+        std::cout << "  dummy = " << dummy << std::endl;
+}
+
